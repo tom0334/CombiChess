f8566e098c499a9a2ba67d3336c0f4bcb61ff2a1
tom0334 <tfriederich@live.nl>
2018-09-03 13:34:16 +0200

    Move entire combichess code to a class
    No more globals!

--- combiChess.py
+++ combiChess.py
@@ -5,191 +5,191 @@ import chess.uci
 import sys
 
 # file names for the engines
-enginePath = "/engines/"
+engineFolder = "/engines/"
 engineFileNames = ["stockfish5", "komodo8", "andscacs"]
 
-# current board status, probably received from UCI position commands
-board = chess.Board()
 
-# after a stop command, ignore
-canceled = False
-
-engines = [None, None, None]
-moves = [None, None, None]
-
-# Statistics for what engines are listened to.
-listenedTo = [0, 0, 0]
-
-
-# on engine done callbacks. The number is the index in the engines array
-def onEngine0Finished(command):
-    onFinished(command, 0)
-
-
-def onEngine1Finished(command):
-    onFinished(command, 1)
-
-
-def onEngine2Finished(command):
-    onFinished(command, 2)
-
-
-def loadEngines():
-    for i in xrange(0, len(engines)):
-        try:
-            engines[i] = chess.uci.popen_engine("./" + enginePath + engineFileNames[i])
-        except:
-            sys.stderr.write("CombiChess Error: could not load the engine at file path:" + engineFileNames[i])
-            sys.stderr.write("\n\nDid you change the script to include the engines you want to use with Combichess?")
-            sys.stderr.write("To do this, open Combichess.py and change the engineFilePaths.\n")
-            sys.exit()
-
-        engines[i].uci()
-        engines[i].ucinewgame()
-
-
-def startEngine(index, callback):
-    engines[index].position(board)
-    command = engines[index].go(movetime=990, async_callback=callback)
-
-
-# mprint("info string started engine " + str(index))
-
-def startEngines():
-    global moves
-    global canceled
-
-    moves = [None, None, None]
+class CombiChess:
+    # after a stop command, ignore
     canceled = False
 
-    startEngine(0, onEngine0Finished)
-    startEngine(1, onEngine1Finished)
-    startEngine(2, onEngine2Finished)
+    engines = [None, None, None]
+    moves = [None, None, None]
 
+    # current board status, probably received from UCI position commands
+    board = chess.Board()
 
-def onFinished(command, index):
-    global canceled
-    global moves
-    global listenedTo
+    # Statistics for what engines are listened to.
+    listenedTo = [0, 0, 0]
 
-    # if this callback is called after the UCI stop command,
-    # we can just ignore it.
-    if canceled:
-        return
+    engineFolder = None
+    engineFileNames = None
 
-    engineMove, ponder = command.result()
+    def __init__(self, engineLocation, engineNames):
+        self.exit = False
+        self.engineFolder = engineLocation
+        self.engineFileNames = engineNames
+        mprint("CombiChess 1.0 by T. Friederich")
 
-    # log the result
-    EngineName = engineFileNames[index]
-    mprint("info string " + EngineName + " says:	       " + str(engineMove))
+    def start(self):
+        for i in xrange(0, len(self.engines)):
+            try:
+                self.engines[i] = chess.uci.popen_engine("./" + engineFolder + engineFileNames[i])
+            except:
+                sys.stderr.write("CombiChess Error: could not load the engine at file path:" + engineFileNames[i])
+                sys.stderr.write(
+                    "\n\nDid you change the script to include the engines you want to use with Combichess?")
+                sys.stderr.write("To do this, open Combichess.py and change the engineFilePaths.\n")
+                sys.exit()
 
-    # set the move in the found moves
-    moves[index] = engineMove
+            self.engines[i].uci()
+            self.engines[i].ucinewgame()
+        self.__mainloop()
 
-    bestmove = None
+    def __mainloop(self):
+        while not self.exit:
+            userCommand = raw_input()
 
-    # if engine 1 and 2 are done, and they agree on a move, do that move
-    if moves[1] != None and moves[1] == moves[2]:
-        mprint("info string listening to children")
-        listenedTo[0] += 1
-        bestmove = moves[1]
+            if userCommand == "uci":
+                mprint("id name TomsCombiChess")
+                mprint("id author Tom Friederich")
+                mprint("uciok")
 
-    # if engine 0 and another agree, do that move
-    elif moves[0] != None and (moves[0] == moves[1] or moves[0] == moves[2]):
-        mprint("info string listening to master and another")
-        listenedTo[1] += 1
-        bestmove = moves[0]
+            if userCommand.startswith("option name"):
+                mprint("uciok")
 
-    # all engines are done and they dont agree. Listen to master
-    elif None not in moves:
-        mprint("info string listening to master")
-        listenedTo[2] += 1
-        bestmove = moves[0]
-    # we dont know our best move yet
-    else:
-        return
+            if userCommand == "isready":
+                mprint("readyok")
 
-    printStats()
+            if userCommand.startswith("go"):
+                self.startEngines()
 
-    canceled = True
-    # stop remaining engines
-    for engine in engines:
-        engine.stop()
+            elif userCommand.startswith("position"):
+                self.handlePosition(userCommand)
 
-    mprint("bestmove " + str(bestmove))
+            elif userCommand == "exit":
+                self.exit = True
+            elif userCommand == "stop":
+                sleep(300)
+                # TODO  fix this
+                mprint(self.moves[0])
+                mprint("readyok")
+                mprint("uciok")
+            else:
+                mprint("unknown command")
 
+    # on engine done callbacks. The number is the index in the engines array
+    def onEngine0Finished(self, command):
+        self.onFinished(command, 0)
 
-# prints stats on how often was listened to master and how often to children
-def printStats():
-    mprint("info string listenStats [C, M+C, M] " + str(listenedTo))
-    totalSum = listenedTo[0] + listenedTo[1] + listenedTo[2]
-    masterSum = listenedTo[1] + listenedTo[2]
-    masterPercent = (float(masterSum) / float(totalSum)) * 100.0
-    mprint("info string Master % " + str(masterPercent))
+    def onEngine1Finished(self, command):
+        self.onFinished(command, 1)
 
+    def onEngine2Finished(self, command):
+        self.onFinished(command, 2)
 
-# handle UCI position command
-def handlePosition(input):
-    try:
-        words = input.split()
-        # word 0 is position
-        if words[1] == "fen":
-            fen = input.split(' ', 2)[2]
-            mprint("")
-            # mprint(fen)
-            board.set_fen(fen)
-        elif words[1] == "startpos":
-            board.reset()
-            for move in words[3:]:  # skip the first two
-                mprint("Adding " + move + " to stack")
-                board.push_uci(move)
+    def startEngine(self, index, callback):
+        self.engines[index].position(self.board)
+        command = self.engines[index].go(movetime=990, async_callback=callback)
+
+    # mprint("info string started engine " + str(index))
+
+    def startEngines(self):
+
+        self.moves = [None, None, None]
+        self.canceled = False
+
+        self.startEngine(0, self.onEngine0Finished)
+        self.startEngine(1, self.onEngine1Finished)
+        self.startEngine(2, self.onEngine2Finished)
+
+    def onFinished(self, command, index):
+
+        # if this callback is called after the UCI stop command,
+        # we can just ignore it.
+        if self.canceled:
+            return
+
+        engineMove, ponder = command.result()
+
+        # log the result
+        EngineName = engineFileNames[index]
+        mprint("info string " + EngineName + " says:	       " + str(engineMove))
+
+        # set the move in the found moves
+        self.moves[index] = engineMove
+
+        bestmove = None
+
+        # if engine 1 and 2 are done, and they agree on a move, do that move
+        if self.moves[1] != None and self.moves[1] == self.moves[2]:
+            mprint("info string listening to children")
+            self.listenedTo[0] += 1
+            bestmove = self.moves[1]
+
+        # if engine 0 and another agree, do that move
+        elif self.moves[0] != None and (self.moves[0] == self.moves[1] or self.moves[0] == self.moves[2]):
+            mprint("info string listening to master and another")
+            self.listenedTo[1] += 1
+            bestmove = self.moves[0]
+
+        # all engines are done and they dont agree. Listen to master
+        elif None not in self.moves:
+            mprint("info string listening to master")
+            self.listenedTo[2] += 1
+            bestmove = self.moves[0]
+        # we dont know our best move yet
         else:
-            mprint("unknown position type")
-    except Exception as e:
-        mprint("something went wrong. Pls try again")
-        mprint(e)
+            return
 
-    mprint(board)
+        self.printStats()
+
+        self.canceled = True
+        # stop remaining engines
+        for engine in self.engines:
+            engine.stop()
+
+        mprint("bestmove " + str(bestmove))
+
+    # prints stats on how often was listened to master and how often to children
+    def printStats(self):
+        mprint("info string listenStats [C, M+C, M] " + str(self.listenedTo))
+        totalSum = self.listenedTo[0] + self.listenedTo[1] + self.listenedTo[2]
+        masterSum = self.listenedTo[1] + self.listenedTo[2]
+        masterPercent = (float(masterSum) / float(totalSum)) * 100.0
+        mprint("info string Master % " + str(masterPercent))
+
+    # handle UCI position command
+    def handlePosition(self, positionInput):
+        try:
+            words = positionInput.split()
+            # word 0 is position
+            if words[1] == "fen":
+                fen = positionInput.split(' ', 2)[2]
+                mprint("")
+                # mprint(fen)
+                self.board.set_fen(fen)
+            elif words[1] == "startpos":
+                self.board.reset()
+                for move in words[3:]:  # skip the first two
+                    mprint("Adding " + move + " to stack")
+                    self.board.push_uci(move)
+            else:
+                mprint("unknown position type")
+        except Exception as e:
+            mprint("something went wrong. Pls try again")
+            mprint(e)
+
+        mprint(self.board)
 
 
+# UTILS
 def mprint(text):
     arg = str(text)
     sys.stdout.write(arg + "\n")
     sys.stdout.flush()
 
 
-# MAIN SCRIPT:
-
-loadEngines()
-exit = False
-
-mprint("CombiChess 1.0 by T. Friederich ")
-while (not exit):
-    userCommand = raw_input()
-
-    if userCommand == "uci":
-        mprint("id name TomsCombiChess")
-        mprint("id author tommie")
-        mprint("uciok")
-
-    if userCommand.startswith("option name"):
-        mprint("uciok")
-
-    if userCommand == "isready":
-        mprint("readyok")
-
-    if userCommand.startswith("go"):
-        startEngines()
-
-    elif userCommand.startswith("position"):
-        handlePosition(userCommand)
-
-    elif userCommand == "exit":
-        exit = True
-    elif userCommand == "stop":
-        sleep(300)
-        mprint(moves[0])
-        mprint("readyok")
-        mprint("uciok")
-    else:
-        mprint("unknown command")
+# MAIN SCRIPT
+combiChess = CombiChess(engineFolder, engineFileNames)
+combiChess.start()
